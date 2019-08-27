e97ca232ea4bea11cf1093ad5a8b48fefde71d27
Carine Ngwekwe <carine@Carines-MacBook-Pro.local>
2019-08-27 18:21:20 -0500

    adding more features

--- public/browser.js
+++ public/browser.js
@@ -1,8 +1,45 @@
+function itemTemplate(item){
+    return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+    <button data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">:&#9997;&#127996;</button>
+    <button data-id="${item._id}"class="delete-me btn btn-danger btn-sm">&#128078;&#127997;</button>
+    </div>
+    </li>` 
+    //initial page load render
+    let ourHTML = items.map(function(item){
+        return itemTemplate(item)
+
+    }).join('')
+    document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+}
+//create feature
+let createField = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", function(e){
+    e.preventDefault()
+    axios.post('/create',{text: createField.value}).then(function(response){
+     //create the html for a new item
+    
+ document.getElementById("item-list").insertAdjacentHTML("beforeend",itemTemplate(response.data))
+ createField.value =""
+ createField.focus ()
+    }).catch(function(){
+        console.log ("you almost got it")
+    })
+
+})
+
 document.addEventListener("click", function(e){
     //Delete feature
 if(e.target.classList.contains("delete-me")){
-    if(confirm("Do you sure &#129318;&#127996;")){
-        axios.post
+    
+    if(confirm("Are you sure you ??")){
+        axios.post('/delete-item',{ id: e.target.getAttribute("data-id")}).then(function(){
+            e.target.parentElement.parentElement.remove()
+     
+        }).catch(function(){
+            console.log ("you almost got it")
+        })
 
     }
 }
@@ -12,7 +49,7 @@ if(e.target.classList.contains("delete-me")){
     if(e.target.classList.contains("edit-me")){
     let userInput = prompt("enter your best favfood", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
    if(userInput){
-    axios.post('/update-item',{text: userInput, id: e.target.getAttribute("data-id")}).then(function(){
+    axios.post('/update',{text: userInput, id: e.target.getAttribute("data-id")}).then(function(){
         e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
  
     }).catch(function(){
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 let express = require('express')
 let app = express()
 let mongodb = require('mongodb')
+let sanitizeHTML = require('sanitize-html')
+let port = process.env.PORT
+if(port == null || port == ""){
+  port = 3000
+}
+
 
 app.use(express.static('public'))
 
 let connectionString = 'mongodb+srv://vic:Overcomer30@@cluster0-b3sxt.mongodb.net/todoAppUser?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true},function(err, client){
 db = client.db()
-app.listen(3000)
+app.listen(port)
 })
 
 app.use(express.json())
 app.use( express.urlencoded({extended: false}))
 
+function passwordProctected(req, res, next){
+ res.set('www-Authenticate', 'Basic realm="favFood-App')
+console.log(req.headers.authorization)
 
+if(req.headers.authorization == "Basic ZmF2Rm9vZC1hcHA6cGFzczEyMzQ="){
+  next()
+}else{
+  res.status(401).send("Authentication required")
+}
+}
+app.use(passwordProctected)
      app.get('/', function(req,res){
         db.collection('items').find().toArray(function(err, items){
          
@@ -21,6 +37,7 @@ app.use( express.urlencoded({extended: false}))
         res.send(`<!DOCTYPE html>
         <html>
         <head>
+        <link rel="stylesheet" href="css/site.css">
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Simple To-Do App</title>
@@ -31,29 +48,22 @@ app.use( express.urlencoded({extended: false}))
             <h1 class="display-4 text-center py-1">favFood-App</h1>
             
             <div class="jumbotron p-3 shadow-sm">
-              <form action="/create" method="POST">
+              <form  id="create-form" action="/create" method="POST">
                 <div class="d-flex align-items-center">
-                  <input  name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                  <input  id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
                   <button class="btn btn-primary">Add food&#x1F60B</button>
                 </div>
               </form>
             </div>
             
-            <ul class="list-group pb-5">
-              
-            ${items.map(function(item){
-              return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-              <span class="item-text">${item.text}</span>
-              <div>
-              <button data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">:&#9997;&#127996;</button>
-              <button data-id="${item._id}"class="delete-me btn btn-danger btn-sm">&#128078;&#127997;</button>
-              </div>
-              </li>`
-
-            }).join('')}
+            <ul id="item-list" class="list-group pb-5">
+           
             </ul>
             
           </div>
+          <script>
+          let items = ${JSON.stringify(items)}
+          </script>
           <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
           <script src="/browser.js"></script>
         </body>
@@ -63,14 +73,21 @@ app.use( express.urlencoded({extended: false}))
    
      })
 app.post('/create',function(req,res){
-    db.collection('items').insertOne({text: req.body.item}, function(){
-       res.redirect('/')
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+    db.collection('items').insertOne({text: safeText}, function(err, info){
+       res.json(info.ops[0])
     })
 })
 app.post('/update',function(req,res){
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)},{$set: {text: req.body.text}},function (){
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)},{$set: {text: safeText}},function (){
     res.send("sucess")
 
   } )
     
+})
+app.post('/delete-item', function(req,res){
+  db.collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)},function(){
+    res.send("success")
+  })
 })
